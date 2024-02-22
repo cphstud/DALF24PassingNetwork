db6186a916131328208f557fa75db502174c96c4
thor <thor@cphbusiness.dk>
2024-02-22 17:09:15 +0100

    basic ui

--- passingNetWork.R
+++ passingNetWork.R
@@ -5,10 +5,28 @@ library(ggsoccer)
 
 
 ui <- fluidPage(
-  
-)
+  sidebarLayout(
+    sidebarPanel(
+      selectInput("team",
+                  label = "Vælg hold",
+                  choices = NULL
+                  ),
+      selectInput("match",
+                  label = "Vælg kamp",
+                  choices = NULL
+                  ),
+      numericInput("minutes",
+                  label = "Vælg kampperiode",
+                  value = 0, step = 10
+                  )
+    ),
+      mainPanel(
+      )
+    )
+  )
 
 server <- function(input, output, session) {
+  
   
 }
 
