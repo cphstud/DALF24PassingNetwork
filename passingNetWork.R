a19de1e0a5f6c073881e646d73cc7d149f610810
thor <thor@cphbusiness.dk>
2024-02-22 23:31:30 +0100

    feature engineering II

--- passingNetWork.R
+++ passingNetWork.R
@@ -39,6 +39,7 @@ server <- function(input, output, session) {
   
   output$passplot <- renderPlot({
     mval <- input$match
+    mval <- "Willem II - Feyenoord _ 5241741"
     match <- str_split(mval,"_")
     mid <- match[[1]][2]
     teams <- match[[1]][1]
@@ -46,19 +47,43 @@ server <- function(input, output, session) {
     
     #home and away teams
     ht <- hamatch[[1]][1]
+    ht <- gsub(" ","",ht)
     at <- hamatch[[1]][2]
+    at <- gsub(" ","",at)
     mid <- gsub(" ","",mid)
     print(mid)
     allPM <- allPasses %>% filter(matchId==mid)
     
     # prep the dataframe
+    
     # split the teams
     allPM1 <- allPM %>% filter(team.name==ht)
-    allPM2 <- allPM %>% filter(opponentTeam.name==at)
+    allPM2 <- allPM %>% filter(team.name==at)
+    # mean location for den som afleverer
+    allPM1gr <- allPM1 %>% group_by(player.name) %>% 
+      mutate(mx=mean(location.x),
+             my=mean(location.y),
+             )  %>% select(player.name,
+                          player.position,
+                          pass.recipient.name,
+                          pass.recipient.position,
+                          mx,
+                          my,
+               pass.endLocation.x,
+               pass.endLocation.y,
+                          team.name
+                          ) %>% unique() 
+    allPM1gr <- allPM1gr %>% group_by(pass.recipient.name) %>% mutate(
+      mrx=mean(pass.endLocation.x),
+      mry=mean(pass.endLocation.y),
+    ) %>% ungroup()
     
+    # now count the number of passes
+    allPM1ct <- allPM1gr %>% group_by(player.name,pass.recipient.name) %>% 
+      mutate(cnt=n()) %>% arrange(desc(cnt)) %>% 
+      select(player.name,cnt,pass.recipient.name,mx,my,mrx,mry,team.name) %>%  unique()
     
-    
-    
+    # now passplot
     
     
   })
