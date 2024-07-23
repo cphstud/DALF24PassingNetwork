5a14b247ede93977b37d29f7fc9fc95ec5406f3c
thor <thor@MacBookbusiness.ihswifi>
2024-07-23 09:37:13 +0200

    dk match only

--- passingNetWork.R
+++ passingNetWork.R
@@ -4,6 +4,7 @@ library(ggplot2)
 library(ggsoccer)
 library(stringr)
 library(jsonlite)
+library(grid)
 
 
 ui <- fluidPage(
@@ -11,15 +12,16 @@ ui <- fluidPage(
     sidebarPanel(
       selectInput("team",
                   label = "Vælg hold",
-                  choices = dteams
-                  ),
-      selectInput("match",
-                  label = "Vælg kamp",
-                  choices = NULL
+                  choices = c("Denmark","Slovenia"),
+                  selected = "Denmark"
                   ),
       numericInput("minutes",
                   label = "Vælg kampperiode",
-                  value = 0, step = 10
+                  value = 1, step = 1
+                  ),
+      numericInput("mincount",
+                  label = "Vælg minimum antal afleveringer",
+                  value = 1, step = 1
                   )
     ),
       mainPanel(
@@ -29,85 +31,65 @@ ui <- fluidPage(
   )
 
 server <- function(input, output, session) {
+  # manuel prep
   dkMatch = fromJSON("data/3930162.json", flatten = T)
-  dkMPasses=dkMatch %>% filter(type.name=="Pass")
+  dkMPassesBU=dkMatch %>% filter(type.name=="Pass")
+  dkMPasses=dkMPassesBU
   dkMPasses$location.x=unlist(lapply(dkMPasses$location, function(x) x[1]))
   dkMPasses$location.y=unlist(lapply(dkMPasses$location, function(x) x[2]))
   dkMPasses$pass.endLocation.x=unlist(lapply(dkMPasses$pass.end_location, function(x) x[1]))
   dkMPasses$pass.endLocation.y=unlist(lapply(dkMPasses$pass.end_location, function(x) x[2]))
-  # hold i dropdown
-  observe({
-    tval <- input$team
-    tval=ifelse(nchar(tval)<2,"Ajax",input$team)
-    choices <- dutchmatches %>% filter(home==tval) %>% select(longinfo)
-    updateSelectInput(session, "match", choices = choices)
-  })
+  dkMPasses$pass.endLocation.y=unlist(lapply(dkMPasses$pass.end_location, function(x) x[2]))
+  dkMPasses <- dkMPasses %>% mutate(period=(minute %/% 10)+1)
+  ht <- "Denmark"
+  at <- "Slovenia"
+  # mean location for den som afleverer
   
   output$passplot <- renderPlot({
-    mval <- input$match
-    mval <- "Willem II - Feyenoord _ 5241741"
-    match <- str_split(mval,"_")
-    mid <- match[[1]][2]
-    teams <- match[[1]][1]
-    hamatch <- str_split(teams," - ")
+    tval <- input$minutes
+    team <- input$team
+    minpass <- input$mincount
     
-    #home and away teams
-    ht <- hamatch[[1]][1]
-    ht <- "Denmark"
-    ht <- gsub(" $","",ht)
-    at <- hamatch[[1]][2]
-    at <- gsub(" $","",at)
-    at <- "Slovenia"
-    mid <- gsub(" ","",mid)
-    print(mid)
-    allPM <- allPasses %>% filter(matchId==mid)
-    allPM <- dkMPasses
-    #saveRDS(allPasses,"allPassesII.rds")
-    
-    # prep the dataframe
-    
-    # split the teams
-    allPM1 <- allPM %>% filter(team.name==ht)
-    allPM2 <- allPM %>% filter(team.name==at)
-    # mean location for den som afleverer
+    dkMPassesG <- dkMPasses %>% filter(period==tval)
+    allPM <- dkMPassesG
+    allPM1 <- allPM %>% filter(team.name==team)
     allPM1gr <- allPM1 %>% group_by(player.name) %>% 
       mutate(mx=mean(location.x),
              my=mean(location.y),
-             )  %>% select(player.name,
-                          #player.position,
-                          pass.recipient.name,
-                          #pass.recipient.position,
-                          mx,
-                          my,
-               pass.endLocation.x,
-               pass.endLocation.y,
-                          team.name
-                          ) %>% unique() 
+      )  %>% select(player.name,
+                    period,
+                    pass.recipient.name,
+                    mx,
+                    my,
+                    pass.endLocation.x,
+                    pass.endLocation.y,
+                    team.name
+      ) %>% unique() 
     allPM1gr <- allPM1gr %>% group_by(pass.recipient.name) %>% mutate(
       mrx=mean(pass.endLocation.x),
       mry=mean(pass.endLocation.y),
     ) %>% ungroup()
-    
     # now count the number of passes
-    allPM1ct <- allPM1gr %>% group_by(player.name,pass.recipient.name) %>% 
-      mutate(cnt=n()) %>% arrange(desc(cnt)) %>% 
+    df2=allPM1gr %>% group_by(player.name,pass.recipient.name) %>% 
+      mutate(cnt=n()) %>% arrange(desc(cnt)) %>% filter(cnt>minpass) %>% unique() %>% 
       select(player.name,cnt,pass.recipient.name,mx,my,mrx,mry,team.name) %>%  unique()
     
-    # now passplot
+    custom_colors <- c("A" = "blue", "B" = "red")
+    custom_labels <- c("A" = "Group A", "B" = "Group B")
     
-    df2=allPM1ct %>% filter(cnt > 3)
     ggplot(df2) +
       annotate_pitch() +
-      geom_segment(aes(x = mx, y = my, xend = mrx, yend = mry),size=df2$cnt/8)+
+      geom_segment(aes(x = mx, y = my, xend = mrx, yend = mry),size=df2$cnt/8,
+                   arrow = arrow(length = unit(0.3, "cm"), type = "closed"))+
       geom_point(aes(x=mx,y=my,color=df2$team.name), size=2)+
       geom_point(aes(x=mrx,y=mry,color=df2$team.name), size=2)+
       geom_text(aes(x=mx,y=my,label=player.name),color="blue",size=2,vjust=-2)+
       geom_text(aes(x=mrx,y=mry,label=pass.recipient.name),color="red",size=2,vjust=2)+
       theme_pitch() +
+      theme(legend.position = "none")+
       direction_label() +
-      ggtitle("Simple passmap", 
-              "ggsoccer example")
-    
+      ggtitle("passmap", df2$team.name )
+      
     
   })
 }
